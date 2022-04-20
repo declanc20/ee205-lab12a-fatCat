869fa5ad5bd28d9fcc530774c779b871e7553d16
declanc20 <declanc@hawaii.edu>
2022-04-19 22:48:27 -1000

    tested < overriden operator.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,18 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
 
+
+    //test conversion functions
+    /*cout<<Weight::convertWeight(5, Weight::POUND, Weight::KILO)<<endl;
+    cout<<Weight::convertWeight(5, Weight::POUND, Weight::SLUG)<<endl;
+    cout<<Weight::convertWeight(5, Weight::POUND, Weight::POUND)<<endl;
+    cout<<Weight::convertWeight(5, Weight::SLUG, Weight::SLUG)<<endl;
+    cout<<Weight::convertWeight(5, Weight::SLUG, Weight::KILO)<<endl;
+    cout<<Weight::convertWeight(5, Weight::SLUG, Weight::POUND)<<endl;
+    cout<<Weight::convertWeight(5, Weight::KILO, Weight::KILO)<<endl;
+    cout<<Weight::convertWeight(5, Weight::KILO, Weight::SLUG)<<endl;
+    cout<<Weight::convertWeight(5, Weight::KILO, Weight::POUND)<<endl;*/
+
     //test all 7 constructors
     Weight myWeight = Weight();
     Weight myWeight2 = Weight(17);
@@ -30,28 +42,18 @@ int main() {
 
 #ifdef Debug
     //myWeight2.setWeight(Weight::UNKNOWN_WEIGHT); //fails because weight can't be "un-kown"
-
+    //myWeight5.setWeight(100); //fails because 100 pounds is more than max weight of 33
+    //myWeight5.setWeight(100, Weight::KILO); //fails because 100 kilos is more than max wieght of 33 pounds
 #endif
 
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
 
-
-    //test conversion functions
-    /*cout<<Weight::convertWeight(5, Weight::POUND, Weight::KILO)<<endl;
-    cout<<Weight::convertWeight(5, Weight::POUND, Weight::SLUG)<<endl;
-    cout<<Weight::convertWeight(5, Weight::POUND, Weight::POUND)<<endl;
-    cout<<Weight::convertWeight(5, Weight::SLUG, Weight::SLUG)<<endl;
-    cout<<Weight::convertWeight(5, Weight::SLUG, Weight::KILO)<<endl;
-    cout<<Weight::convertWeight(5, Weight::SLUG, Weight::POUND)<<endl;
-    cout<<Weight::convertWeight(5, Weight::KILO, Weight::KILO)<<endl;
-    cout<<Weight::convertWeight(5, Weight::KILO, Weight::SLUG)<<endl;
-    cout<<Weight::convertWeight(5, Weight::KILO, Weight::POUND)<<endl;*/
-
-
-
-
-
-
-
+    bool overrideTester = Weight(19)<Weight(17);
+    cout<< overrideTester << endl;
+    overrideTester = Weight(17)<Weight(19);
+    cout<< overrideTester << endl;
 
 
 
